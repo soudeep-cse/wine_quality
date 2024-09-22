fd24c67707522d98dfa7fe06b58a5816bda276d8
Shahriar Soudeep <83727463+soudeep-cse@users.noreply.github.com>
2024-09-22 22:26:13 +0600

    Add files via upload

---
+++ Wine_prediction_WebApp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 13 00:55:37 2024
+
+@author: ASUS
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+load_model=pickle.load(open())
